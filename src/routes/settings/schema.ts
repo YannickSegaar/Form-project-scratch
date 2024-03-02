a1eb4354c5f2a158abe4e80f9f2b01c2c722531c
YRS <yannickromainsegaar@gmail.com>
2024-03-02 12:22:03 +0100

    shadcn-svelte docs gevolgd en alle relevante files in een settings folder geplaatst, hierdoor werkt de form nu wel

---
+++ src/routes/settings/schema.ts
@@ -0,0 +1,9 @@
+//YRS: shadcn-svelte docs form creation
+
+import { z } from "zod";
+ 
+export const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+ 
+export type FormSchema = typeof formSchema;
