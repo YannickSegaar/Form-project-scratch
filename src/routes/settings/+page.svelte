8818c6aae3fb4a3330b27670e9a9bbda3c6a7330
YRS <yannickromainsegaar@gmail.com>
2024-03-02 16:22:22 +0100

    Form docs knowledge base toegevoegd als plain text file

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -5,3 +5,4 @@
   </script>
    
   <SettingsForm data={data.form} />
+  
--- src/routes/settings/schema.ts
+++ src/routes/settings/schema.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
  
 export const formSchema = z.object({
   username: z.string().min(2).max(50),
+  email: z.string({ required_error: "Please select an email to display" }).email()
 });
  
 export type FormSchema = typeof formSchema;
