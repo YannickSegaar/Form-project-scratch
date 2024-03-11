08fd6756aaf736622c8f775b2cd6c39b46667fae
YRS <yannickromainsegaar@gmail.com>
2024-03-11 12:30:35 +0100

    Tooltip branch niet gelukt, ik maak een nieuwe branch om de playground en settings directories samen te voegen

---
+++ src/routes/combined/+page.svelte
@@ -0,0 +1,10 @@
+<!-- YRS: LET OP DIT IS EEN COMBINED FILE -->
+
+<script lang="ts">
+    import type { PageData } from "./$types";
+    import CombinedForm from "./combined-form.svelte";
+    export let data: PageData;
+  </script>
+   
+  <CombinedForm data={data.form} />
+
