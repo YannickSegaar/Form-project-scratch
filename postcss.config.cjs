8c9e1d7f9e9873be187b1ce5ec9b3e564b566f0a
YRS <yannickromainsegaar@gmail.com>
2024-03-12 14:21:42 +0100

    Gefaald om skeleton goed te importeren

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,13 +3,9 @@ const autoprefixer = require('autoprefixer');
 const postcssNesting = require('postcss-nesting');
 
 const config = {
-    plugins: [
-        // Include postcss-nesting before Tailwind CSS for nesting support
-        postcssNesting,
-        tailwindcss(),
-        // Other plugins like autoprefixer can follow after
-        autoprefixer,
-    ]
+	plugins: [// Include postcss-nesting before Tailwind CSS for nesting support
+    postcssNesting, // Other plugins like autoprefixer can follow after
+    tailwindcss(), //Some plugins, like tailwindcss/nesting, need to run before Tailwind, tailwindcss(), //But others, like autoprefixer, need to run after, autoprefixer, autoprefixer]
 };
 
 module.exports = config;
